f8562fe80d04137a56b9a13f5a77fa80ac12310e
ambienteo3 <ao3ambienteozono@gmail.com>
2024-04-01 13:14:10 -0300

    add new laoders

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,16 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'budle.js',
-        path: path.resolve(__dirname, 'build'),
+        path: path.resolve(__dirname, 'dist'),
         publicPath: '/',
     },
     plugins: [
         new HtmlWebpackPlugin({ template: './public/index.html' })
-    ]
+    ],
+    module: {
+        rules: [
+            { test: /\.(js|jsx)$/, use: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.css$/, use: 'css-loader' },
+        ]
+    }
 }
