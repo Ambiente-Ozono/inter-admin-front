8d7dd90ad2763388e7f99a660e3e475fbec6e3fa
ambienteo3 <ao3ambienteozono@gmail.com>
2024-04-01 12:17:16 -0300

    add plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'budle.js',
         path: path.resolve(__dirname, 'dist'),
-    }
+        publicPath: '/',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: './public/index.html' })
+    ]
 }
